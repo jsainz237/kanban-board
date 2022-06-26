39e9008606293af35e959de74749dd55e886b163
Jesse Sainz <jsainz237@gmail.com>
2022-06-26 15:59:42 -0500

    redux persistance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components';
 import { SSRProvider } from 'react-bootstrap';
 import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
-import { store } from '../state';
+import { persistor, store } from '../state';
 import { GlobalStyle, theme } from '../styles/theme';
 import '../styles/globals.scss'
 
@@ -15,10 +16,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SSRProvider>
       <ReduxProvider store={store}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </PersistGate>
       </ReduxProvider>
     </SSRProvider>
   );
--- state/index.ts
+++ state/index.ts
@@ -1,13 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import cardsReducer from './cards.reducer'
 import columnsReducer from './columns.reducer'
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, combineReducers({
+  columnsState: columnsReducer,
+  cardsState: cardsReducer,
+}));
+
 export const store = configureStore({
-  reducer: {
-    columnsState: columnsReducer,
-    cardsState: cardsReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
