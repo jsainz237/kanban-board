9260adafcd96a786a036bbfc26edd6f53d8a5fad
Jesse Sainz <jsainz237@gmail.com>
2022-06-26 21:50:46 -0500

    fix form drag issue

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -28,42 +28,7 @@ export const Card = React.forwardRef<{}, DraggableProps>(({
     columnId,
     style,
     ...props
-}, ref) => (
-    <>
-        <Styled.Card ref={ref as any} style={style} {...props as any}>
-            <div>
-                <Styled.CardHeader>
-                    <div className='title'>{name}</div>
-                    <OptionsDropdown
-                        id={id}
-                        name={name}
-                        description={description}
-                        createdDate={createdDate}
-                        status={status}
-                        columnId={columnId}
-                    />
-                </Styled.CardHeader>
-                <Styled.DescriptionContainer>
-                    {description}
-                </Styled.DescriptionContainer>
-            </div>
-            <Styled.FooterContainer>
-                <div className='date'>
-                    {new Date(createdDate).toLocaleDateString()}
-                </div>
-                <Styled.StatusLabel status={status}>
-                    {status}
-                </Styled.StatusLabel>
-            </Styled.FooterContainer>
-        </Styled.Card>
-    </>
-))
-Card.displayName = 'Card';
-
-const OptionsDropdown: React.FC<Props> = ({
-    columnId,
-    ...cardValues
-}) => {
+}, ref) => {
     const [show, setShow] = useState<boolean>(false);
     const dispatch = useAppDispatch();
 
@@ -76,45 +41,68 @@ const OptionsDropdown: React.FC<Props> = ({
     const onArchive = () => {
         dispatch(deleteCard({
             columnId,
-            cardId: cardValues.id,
+            cardId: id,
         }));
     }
 
     const onSubmit = (editedValues: CardDto) => {
         dispatch(editCard({
             columnId,
-            cardId: cardValues.id,
+            cardId: id,
             card: editedValues,
         }))
     }
 
     return (
         <>
-            <Dropdown style={{ pointerEvents: 'fill' }}>
-                <Dropdown.Toggle as={CardOptionsToggle} />
-                <Styled.DropdownMenu variant='dark'>
-                    <Styled.DropdownItem onClick={onEdit} className='edit-btn'>
-                        <FontAwesomeIcon style={{ marginRight: 8 }} icon={faPen} />
-                        Edit task
-                    </Styled.DropdownItem>
-                    <Dropdown.Divider style={{ margin: '0.5rem' }} />
-                    <Styled.DropdownItem onClick={onArchive} className='archive-btn'>
-                        <FontAwesomeIcon style={{ marginRight: 8 }} icon={faTrash} />
-                        Archive
-                    </Styled.DropdownItem>
-                </Styled.DropdownMenu>
-            </Dropdown>
+            <Styled.Card ref={ref as any} style={style} {...props as any}>
+                <div>
+                    <Styled.CardHeader>
+                        <div className='title'>{name}</div>
+                        <Dropdown onMouseDown={e => { console.log("HELLO" ); e.stopPropagation() }} style={{ pointerEvents: 'fill' }}>
+                            <Dropdown.Toggle as={CardOptionsToggle} />
+                            <Styled.DropdownMenu variant='dark'>
+                                <Styled.DropdownItem onClick={onEdit} className='edit-btn'>
+                                    <FontAwesomeIcon style={{ marginRight: 8 }} icon={faPen} />
+                                    Edit task
+                                </Styled.DropdownItem>
+                                <Dropdown.Divider style={{ margin: '0.5rem' }} />
+                                <Styled.DropdownItem onClick={onArchive} className='archive-btn'>
+                                    <FontAwesomeIcon style={{ marginRight: 8 }} icon={faTrash} />
+                                    Archive
+                                </Styled.DropdownItem>
+                            </Styled.DropdownMenu>
+                        </Dropdown>
+                    </Styled.CardHeader>
+                    <Styled.DescriptionContainer>
+                        {description}
+                    </Styled.DescriptionContainer>
+                </div>
+                <Styled.FooterContainer>
+                    <div className='date'>
+                        {new Date(createdDate).toLocaleDateString()}
+                    </div>
+                    <Styled.StatusLabel status={status}>
+                        {status}
+                    </Styled.StatusLabel>
+                </Styled.FooterContainer>
+            </Styled.Card>
 
             <CardModal
                 show={show}
                 type="edit"
                 handleClose={handleClose}
                 onSubmit={onSubmit}
-                initialValues={cardValues}
+                initialValues={{
+                    name,
+                    description,
+                    status,
+                }}
             />
         </>
     );
-};
+});
+Card.displayName = 'Card';
 
 const CardOptionsToggle = React.forwardRef<any, any>(({ onClick }, ref) => (
     <div 
